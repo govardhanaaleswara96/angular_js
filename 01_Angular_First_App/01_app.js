6619159bdaa7492c81e9d124e7a0ffbb993e133d
govardhanaaleswara96 <govardhanaaleswara96@gmail.com>
2018-10-09 21:53:26 +0530

    first commit

---
+++ 01_Angular_First_App/01_app.js
@@ -0,0 +1,8 @@
+
+// create angler js module
+var app = angular.module('FirstApp',[]);
+//udemy
+// create controller
+app.controller('FirstAppCtrl',function($scope){
+    $scope.user = '';
+});
---
+++ 02_Angular_Ng_Init_Ng_If/02_app.js
@@ -0,0 +1,7 @@
+// create anguler module
+var app = angular.module('AgeSelectionApp',[]);
+
+// create a controller
+app.controller('AgeSelectionAppCtrl',function($scope){
+   $scope.check =null;
+});
